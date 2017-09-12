d44403f1700feec37c00310e6371cb6bcb9996f2
gabrielx52 <gabriel.meringolo@gmail.com>
2017-09-11 18:35:26 -0700

    Made project object constructor.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 'use strict';
 
-function Project(name, url, about) {
+function Project(name, url, about, image) {
   this.name = name;
   this.url = url;
   this.about = about;
-  this.render = function render(){
-    //render things
+  this.image = image;
+  this.createAppend = function(elementType, parentNode) {
+    return parentNode.appendChild(document.createElement(elementType));
+  };
+  this.render = function() {
+    var anchor = document.getElementById('projectSection');
+    var div = this.createAppend('div', anchor);
+    var a = this.createAppend('a', div);
+    var img = this.createAppend('img', div);
+    var p = this.createAppend('p', div);
+    a.setAttribute('href', this.url);
+    a.innerText = this.name;
+    img.src = this.image;
+    p.innerText = this.about;
   }
+  this.render();
 }
+
+var busMaul = new Project('Busmall', 'https://github.com/gabrielx52/bus-mall', 'About busmall', '../imgs/busMall.png')
