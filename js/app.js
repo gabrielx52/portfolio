4bf8cb761fb8994f7562a9d2f7c1bc92b2351fa9
gabrielx52 <gabriel.meringolo@gmail.com>
2017-09-11 17:37:25 -0700

    Started JS and HTML scripts.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function Project(name, url, about) {
+  this.name = name;
+  this.url = url;
+  this.about = about;
+  this.render = function render(){
+    //render things
+  }
+}
