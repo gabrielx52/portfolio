5269758b0b6b552007d6a4ef508c702baf615b8e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-09-11 21:05:14 -0700

    Starting to style.

--- js/app.js
+++ js/app.js
@@ -5,15 +5,17 @@ function Project(name, url, about, image) {
   this.url = url;
   this.about = about;
   this.image = image;
-  this.createAppend = function(elementType, parentNode) {
-    return parentNode.appendChild(document.createElement(elementType));
+  this.createAppend = function(elementType, parentNode, className) {
+    var node = parentNode.appendChild(document.createElement(elementType));
+    node.className = className;
+    return node;
   };
   this.render = function() {
     var anchor = document.getElementById('projectSection');
-    var div = this.createAppend('div', anchor);
-    var a = this.createAppend('a', div);
-    var img = this.createAppend('img', div);
-    var p = this.createAppend('p', div);
+    var div = this.createAppend('div', anchor, 'projectBlock');
+    var a = this.createAppend('a', div, 'projectLink');
+    var img = this.createAppend('img', div, 'projectPic');
+    var p = this.createAppend('p', div, 'projectAbout');
     a.setAttribute('href', this.url);
     a.innerText = this.name;
     img.src = this.image;
