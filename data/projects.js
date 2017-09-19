8b5dcd2fe3bddbd3dd9e4cffd1394ed7e86d333e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-09-18 19:11:39 -0700

    Removed old data file, after transfering to JSON.

--- data/projects.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var projects = [
-  {name: 'Salmon Cookies', url: 'https://github.com/gabrielx52/cookie-stand',about: 'About Salmon Cookies' ,image: '../imgs/salmonCookies.png'},
-  {name: 'Bus Mall', url: 'https://github.com/gabrielx52/bus-mall',about: 'About Bus Maul' ,image: '../imgs/busMaul.png'},
-  {name: 'Tennis Anyone?', url: 'https://github.com/gabrielx52/tennis_anyone',about: 'About Tennis Anyone?' ,image: '../imgs/tennisAnyone.png'}
-];
