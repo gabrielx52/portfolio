9b2856eaf0938263d08449e70d600b5b95e06ce8
gabrielx52 <gabriel.meringolo@gmail.com>
2017-09-25 10:37:14 -0700

    Wrapped app.js in an IIFE. Started to change vars to const/let.

--- public/js/app.js
+++ public/js/app.js
@@ -1,56 +1,64 @@
 'use strict';
 
-function Project(rawDataObj) {
-  this.name = rawDataObj.name;
-  this.url = rawDataObj.url;
-  this.about = rawDataObj.about;
-  this.image = rawDataObj.image;
-  Project.all.push(this);
-}
+const APP = {};
+
+(function(module){
+  function Project(rawDataObj) {
+    this.name = rawDataObj.name;
+    this.url = rawDataObj.url;
+    this.about = rawDataObj.about;
+    this.image = rawDataObj.image;
+    Project.all.push(this);
+  }
 
 
-Project.fetchAll = function() {
-  navHandler();
-  if (localStorage.rawData) {
-    Project.initializeProjects(JSON.parse(localStorage.rawData));
-  } else {
-    $.get('data/projects.json', function(response) {
-      localStorage.setItem('rawData', JSON.stringify(response))
-      Project.initializeProjects(response);
+  Project.fetchAll = function() {
+    navHandler();
+    if (localStorage.rawData) {
+      Project.initializeProjects(JSON.parse(localStorage.rawData));
+    } else {
+      $.get('data/projects.json', function(response) {
+        localStorage.setItem('rawData', JSON.stringify(response))
+        Project.initializeProjects(response);
+      })
+    }
+  }
+
+  Project.prototype.toHtml = function() {
+    var projectTemplate = Handlebars.compile($('#article-template').html());
+    return projectTemplate(this);
+  }
+
+  Project.all = [];
+
+  Project.initializeProjects = function(rawData){
+    rawData.map(element => new Project(element))
+    listProjects();
+  }
+
+  function colorPicker(){
+    var colors = ['#f58b71', '#e6b25d', '#519d9d', '#9c9746'];
+    return colors[Math.round(Math.random() * (colors.length - 1) + 1)];
+  }
+
+  $('#hamburger').on('click', function(){
+    $('ul').show();
+  })
+
+  var navHandler = function(){
+    $('article').hide();
+    $('#homeBlock').show();
+    $('.tab').on('click', function(){
+      $('article').hide();
+      $('ul').hide();
+      $('#' + $(this).attr('data-content')).show();
     })
   }
-}
 
-Project.prototype.toHtml = function() {
-  var projectTemplate = Handlebars.compile($('#article-template').html());
-  return projectTemplate(this);
-}
 
-Project.all = [];
 
-Project.initializeProjects = function(rawData){
-  rawData.map(element => new Project(element))
-  listProjects();
-}
-
-var navHandler = function(){
-  $('article').hide();
-  $('#homeBlock').show();
-  $('.tab').on('click', function(){
-    $('article').hide();
-    $('#' + $(this).attr('data-content')).show();
-  })
-}
-
-function listProjects(){
-  Project.all.forEach(project => $('#projectSection').prepend(project.toHtml()));
-}
-
-function colorPicker(){
-  var colors = ['#f58b71', '#e6b25d', '#519d9d', '#9c9746'];
-  return colors[Math.round(Math.random() * (colors.length - 1) + 1)];
-}
-
-$('#hamburger').on('click', function(){
-  $('ul').show();
-})
+  function listProjects(){
+    Project.all.forEach(project => $('#projectSection').prepend(project.toHtml()));
+  }
+  module.Project = Project;
+})(APP)
